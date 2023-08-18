327dd0b806561be7cb2a44097362822586d31815
Nick Rabone <n.rabone72@aol.com>
2023-08-18 23:16:53 +0100

    3rd commit - Added Header + Footer with Bootstrap.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+
 const App = () => {
-  return <h1>Testing - Welcome to ProShop</h1>
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )  
 }
 
 export default App
