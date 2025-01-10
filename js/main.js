eef7ec6a9340db1be0ef3c8f19091046fe758081
Nimira43 <n.rabone72@aol.com>
2025-01-10 10:09:03 +0000

    There is some really strange scrolling going on here. Commented out the font styles in styles.css and checking code.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ import { exteriorImages, interiorImages } from './imageMapping'
 const basePrice = 57835
 let currentPrice = basePrice
 let selectedColour = 'Stealth Grey' 
+
+const handleScroll = () => {
+  const atTop = window.scrollY === 0 
+  topBar.classList.toggle('visible-bar', atTop)
+  topBar.classList.toggle('hidden-bar', !atTop)
+}
+
+window.addEventListener('scroll', () => requestAnimationFrame(handleScroll))
