d9e008ad29b4deddde61dc6159b113cce8cd7bd5
Nimira43 <n.rabone72@aol.com>
2025-01-10 08:26:50 +0000

    Created variable inside main.js for pricing and selected colour

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 import { topBar, exteriorImage, interiorImage, exteriorColourSection, interiorColourSection, wheelsButtonsSection, performanceBtn, fullSelfDrivingCheckbox,totalPriceElement, downPaymentElement, monthlyPaymentElement, accessoryCheckboxes } from './variables'
+
+const basePrice = 57835
+let currentPrice = basePrice
+let selectedColour = 'Stealth Grey' 
