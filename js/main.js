b56a89f941d00303b06f101dfdc976cdb0578bf2
Nimira43 <n.rabone72@aol.com>
2025-01-10 08:20:42 +0000

    Brought those variables into main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-
+import { topBar, exteriorImage, interiorImage, exteriorColourSection, interiorColourSection, wheelsButtonsSection, performanceBtn, fullSelfDrivingCheckbox,totalPriceElement, downPaymentElement, monthlyPaymentElement, accessoryCheckboxes } from './variables'
