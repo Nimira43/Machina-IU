b3e293611eeced47255cb75cf2936757db1ae382
Nimira43 <n.rabone72@aol.com>
2025-01-10 08:40:04 +0000

    Created object variables for selected options and pricing

---
+++ js/objects.js
@@ -0,0 +1,16 @@
+const selectedOptions = {
+  'Enhanced Wheels': false,
+  'Enhanced Performance': false,
+  'Self-Driving': false,
+}
+
+const pricing = {
+  'Enhanced Wheels': 2600,
+  'Enhanced Performance': 5200,
+  'Self-Driving': 9500,
+  'Accessories' : {
+    'Sunshade' : 115,
+    'All Weather Interior': 250,
+    'Central Console Trays': 40,
+  }
+}
